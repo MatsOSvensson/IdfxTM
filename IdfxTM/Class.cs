43f1d98465b9f49bd961938e222b43e374222699
Mats Svensson <mats.o.svensson@hotmail.com>
2018-12-09 21:00:10 +0100

    Add project files.

---
+++ IdfxTM/Class.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// 
+    /// </summary>
+	public class $safeitemrootname$ : TMBaseNode
+	{
+        /// <summary>
+        /// 
+        /// </summary>
+        public $safeitemrootname$() : base(1,"")
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual CoreInit() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual CoreMeasure() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual CoreExit() { }
+    }
+}
